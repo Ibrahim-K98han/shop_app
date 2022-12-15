c6e00bc06cdaaadc4c522610ed38d1958154df83
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-12-16 01:16:00 +0600

    Initial commit

---
+++ lib/widgets/grid_item.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class GridItems extends StatelessWidget {
+  var pNames = [
+    'Apple Watch -M2',
+    'White Tshirt',
+    'Nike Shoe',
+    'Ear Headphone'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: pNames.length,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 0.7,
+        crossAxisCount: 2,
+      ),
+      itemBuilder: (context, index){
+        return InkWell(
+          onTap: (){},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.redAccent,
+              boxShadow:[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  spreadRadius: 2
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '30% off',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.white
+                        ),
+                      ),
+                      Icon(Icons.favorite, color: Colors.white,)
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('assets/images/${pNames[index]}.png',
+                    height: 100,width: 100,),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          pNames[index],
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Text(
+                              '\$1000',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            SizedBox(width: 20,),
+                            Text(
+                              '\$1500',
+                              style: TextStyle(
+                                decoration: TextDecoration.lineThrough,
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
